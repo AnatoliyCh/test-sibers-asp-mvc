0161aab9e85ee3b554bdebc05c8baa56a138fb77
Anatoly <googlsocial97@gmail.com>
2020-04-01 04:51:17 +0700

    create context

---
+++ web.server/0_DataAccessLayer/DataBase/ProjectDataContext.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DataBase
+{
+    public class ProjectDataContext : DbContext
+    {
+        public ProjectDataContext() : base("ProjectData") { }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ web.server/0_DataAccessLayer/Model/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Model
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CustomerName { get; set; } // компания-заказчик
+        public string ExecutingCompany { get; set; } // компания-исполнитель
+        public ICollection<Employee> Employees { get; set; } // сотрудники
+        public Employee ProjectManager { get; set; } // руководитель проекта
+        public ICollection<Employee> Executors { get; set; } // исполнители проекта
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        private int Priority { get; set; }
+
+        public Project()
+        {
+            Employees = new List<Employee>();
+            Executors = new List<Employee>();
+        }
+    }
+}
