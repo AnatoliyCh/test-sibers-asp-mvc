c92f78319783e2d3b90105fb274adae3b7c979c9
Anatoly <googlsocial97@gmail.com>
2020-04-02 01:49:06 +0700

    Data Access Layer end

---
+++ web.server/0_DataAccessLayer/Repository/UnitOfWork.cs
@@ -0,0 +1,56 @@
+﻿using DataAccessLayer.Context;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ProjectDataContext dataContext;
+        private ProjectRepository projectRepository;
+        private EmployeeRepository employeeRepository;
+
+        public UnitOfWork(string connectionString) => dataContext = new ProjectDataContext();
+        public IRepository<Project> Projects
+        {
+            get
+            {
+                if (projectRepository == null) projectRepository = new ProjectRepository(dataContext);
+                return projectRepository;
+            }
+        }
+
+        public IRepository<Employee> Employees
+        {
+            get
+            {
+                if (employeeRepository == null) employeeRepository = new EmployeeRepository(dataContext);
+                return employeeRepository;
+            }
+        }
+
+        public void Save() => dataContext.SaveChanges();
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing) dataContext.Dispose();
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
