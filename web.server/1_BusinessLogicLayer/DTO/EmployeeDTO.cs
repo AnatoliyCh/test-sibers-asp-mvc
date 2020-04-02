f673de5a8cdbdb0a6fddfeca0f37675cc4569a7f
Anatoly <googlsocial97@gmail.com>
2020-04-02 20:22:03 +0700

    add DTO

---
+++ web.server/1_BusinessLogicLayer/DTO/EmployeeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.DTO
+{
+    public class EmployeeDTO
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string Email { get; set; }
+        public int EmployeeInProjects { get; set; } // кол-во проектов в которых ОН сотрудник
+        public int ExecutorInProjects { get; set; } // кол-во проектов в которых ОН исполнитель
+    }
+}
---
+++ web.server/1_BusinessLogicLayer/DTO/ProjectDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.DTO
+{
+    public class ProjectDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CustomerName { get; set; } // компания-заказчик
+        public string ExecutingCompany { get; set; } // компания-исполнитель        
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Priority { get; set; } // приоритет, 0 - min
+        public EmployeeDTO ProjectManager { get; set; } // руководителя проекта
+        public int Employees { get; set; } // сотрудники
+        public int Executors { get; set; } // исполнители проекта
+    }
+}
